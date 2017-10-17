a8da4fd233a5e61acabe8bc597566453e4df4b80
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-10-17 13:17:43 +0300

    WinForms MDI sample is added.

---
+++ App/ParentForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WinAppToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WinApp form = new WinApp
+            {
+                MdiParent = this
+            };
+
+            form.Show();
+        }
+
+        private void UserInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UserInfo form = new UserInfo
+            {
+                MdiParent = this
+            };
+
+            form.Show();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+    }
+}
--- App/UserInfo.Designer.cs
+++ App/UserInfo.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace App
 {
-    partial class Form1
+    partial class UserInfo
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "UserInfo";
         }
 
         #endregion
     }
 }
-
